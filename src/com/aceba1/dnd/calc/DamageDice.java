bc45e904de9aeadcc192ee60a22fa0f2ba7a4708
Arniel <aceba1@outlook.com>
2020-10-11 14:31:36 -0400

    Implement regex check

--- src/com/aceba1/dnd/calc/DamageDice.java
+++ src/com/aceba1/dnd/calc/DamageDice.java
@@ -6,6 +6,9 @@ import java.util.Random;
 import java.util.stream.Collectors;
 
 public class DamageDice {
+
+  public final static String diceRegex = "^(0*[1-9]+[0-9]*|)d(0*[1-9]+[0-9]*)$";
+
   private final static Random random = new Random();
 
   private String dice;
@@ -27,27 +30,29 @@ public class DamageDice {
     return dice;
   }
 
+  public static boolean testFormat(String dice) {
+    return dice.matches(diceRegex);
+  }
+
   public static String format(int count, int range) {
     return (count > 1 ? count : "") + "d" + range;
   }
 
   public static int roll(String dice) {
-    try {
-      String[] sides = dice.split("d", 2);
-
-      int count = 1;
-      if (sides[0].length() != 0)
-        count = Integer.parseInt(sides[0]);
-      int range = Integer.parseInt(sides[1]);
-
-      int sum = 0;
-      for (int i = 0; i < count; i++) {
-        sum += random.nextInt(range) + 1;
-      }
-      return sum;
-
-    } catch(Exception e) {
+    if (!testFormat(dice))
       throw new MissingFormatArgumentException("Input not of format '{X?}d{Y}");
+
+    String[] sides = dice.split("d", 2);
+
+    int count = 1;
+    if (sides[0].length() != 0)
+      count = Integer.parseInt(sides[0]);
+    int range = Integer.parseInt(sides[1]);
+
+    int sum = 0;
+    for (int i = 0; i < count; i++) {
+      sum += random.nextInt(range) + 1;
     }
+    return sum;
   }
 }
